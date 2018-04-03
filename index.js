fc22e2bb9d99fac0981ea06ad7822c9f603269e0
Jaiman Chin <jaiman.chin@gmail.com>
2018-04-03 00:27:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ function nestedTarget() {
 }
 
 function increaseRankBy(n) {
+  
   var rankedLists = document.querySelectorAll('.ranked-list li')
   for (var i=0; i<rankedLists.length; i++) {
     rankedLists[i].innerHTML = parseInt(rankedLists[i].innerHTML)+n 
